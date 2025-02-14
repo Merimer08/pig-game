f397e6a55b4620d86ca31aada3835ded73cf1838
Merimer08 <marialolu08@gmail.com>
2025-02-14 12:34:41 +0100

    init DOM elements

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
-import './style.css'
+import "./style.css";
 
-
-document.querySelector('#app').innerHTML = `
-   <main>
+document.querySelector("#app").innerHTML = `
+    <main>
       <section class="player player--0 player--active">
         <h2 class="name" id="name--0">Player 1</h2>
         <p class="score" id="score--0">43</p>
         <div class="current">
           <p class="current-label">Current</p>
-          <p class="current-score" id="current--0">0</p>
+          <p class="current-score" id="current--0">3</p>
         </div>
       </section>
       <section class="player player--1">
@@ -16,7 +15,7 @@ document.querySelector('#app').innerHTML = `
         <p class="score" id="score--1">24</p>
         <div class="current">
           <p class="current-label">Current</p>
-          <p class="current-score" id="current--1">0</p>
+          <p class="current-score" id="current--1">5</p>
         </div>
       </section>
 
@@ -25,52 +24,45 @@ document.querySelector('#app').innerHTML = `
       <button class="btn btn--roll">🎲 Roll dice</button>
       <button class="btn btn--hold">📥 Hold</button>
     </main>
-`
 
-;
+`;
 
-//select DOM elements
+// variables de estado en JS y selectores DOMXS
 
-//variable activePlayer
-const SelectionPlayer0 = document.querySelector('.player--0');
-const SelectionPlayer1 = document.querySelector('.player--1');
+// activePlayer -> variable de estado en JS
+const sectionPlayer0 = document.querySelector(".player--0");
+const sectionPlayer1 = document.querySelector(".player--1");
+// score = [0,0] -> variable de estado en JS
+const score0 = document.querySelector("#score--0");
+const score1 = document.querySelector("#score--1");
 
-// score = [0,0] -> Variable de estado en JS
+// current -> variable de estado en JS
+const currentScore0 = document.querySelector("#current--0");
+const currentScore1 = document.querySelector("#current--1");
 
-const score0 = document.querySelector('.score--0');
-const score1 = document.querySelector('.score--1');
-//current ->variable de estado en JS
-const currentScore0 = document.querySelector('.current--0');
-const currentScore1 = document.querySelector('.current--1');
+const btnNew = document.querySelector(".btn--new");
+const btnHold = document.querySelector(".btn--hold");
+const btnRoll = document.querySelector(".btn--roll");
 
-
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
-
-const imgDice = document.querySelector('.dice');
+const imgDice = document.querySelector(".dice");
 
 let score, currentScore, activePlayer;
 
-const inirData = () => {
+const initData = () => {
+  // init state variables
+  score = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  // init DOM elements
+console.log(imgDice, "imgDice");
 
-  //init state variables
-    score = [0, 0];
-    currentScore = 0;
-    activePlayer = 0;
+  imgDice.classList.add("hidden");
 
-    //init DOM
-  console.log(score0, "score0");
-  console.log(score1, "score1");
 
-  console.log(currentScore0, "currentScore0");
-  console.log(currentScore1, "currentScore1");
-
-  
-    score0.textContent = 0;
-    score1.textContent = 0;
-    currentScore0.textContent = 0;
-    currentScore1.textContent = 0;
+  score0.textContent = 0;
+  score1.textContent = 0;
+  currentScore0.textContent = 0;
+  currentScore1.textContent = 0;
 };
 
-inirData()
+initData();
